fa94a03415aee164c39484b656086665895f1d14
Michael Bayless <michael@yoga>
2016-10-08 00:52:58 -0400

    started

---
+++ src/backEnd/RegisterFile.java
@@ -0,0 +1,17 @@
+package backEnd;
+
+public class RegisterFile {
+	
+	private int[] data;
+	
+	 public RegisterFile() {
+		 this.data = new int[30]; }
+	 
+	 public byte getByte(int register, int position) {
+		 return (byte) (this.data[register] >> (position << 3)); }
+	 
+	 public short getWord(int register, int position) {
+		 return (short) (this.data[register] >> (position << 4)); }
+	 
+	 public int getInt(int register) {
+		 return this.data[register]; }}
