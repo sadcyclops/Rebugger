0c15191f8e34f47dc6e8c6eabc6f46f573d388a6
wdarvin <wesdarvin@gmail.com>
2016-10-08 01:38:43 -0400

    fixed set in register file

--- src/backEnd/RegisterFile.java
+++ src/backEnd/RegisterFile.java
@@ -1,31 +1,36 @@
 package backEnd;
 
 public class RegisterFile {
+	final static int BYTE = 255;
+	final static int WORD = 65536;
 	
 	private int[] data;
-	
-	 public RegisterFile() {
-		 this.data = new int[30]; }
-	 
-	 public byte getByte(int register, int position) {
-		 return (byte) (this.data[register] >> (position << 3)); }
-	 
-	 public short getWord(int register, int position) {
-		 return (short) (this.data[register] >> (position << 4)); }
-	 
-	 public int getInt(int register) {
-		 return this.data[register]; }
-	 
-	 public void setByte(int register, int value){
-		 this.data[register] = value;
-	 }
-	 
-	 public void setWord(int register, short value){
-		 this.data[register] = value;
-		 
-	 }
-	 
-	 public void setByte(int register, byte value){
-		 this.data[register] = value;
-	 }
+
+	public RegisterFile() {
+		this.data = new int[30];
+	}
+
+	public byte getByte(int register, int position) {
+		return (byte) (this.data[register] >> (position << 3));
+	}
+
+	public short getWord(int register, int position) {
+		return (short) (this.data[register] >> (position << 4));
+	}
+
+	public int getInt(int register) {
+		return this.data[register];
+	}
+
+	public void setByte(int register, int value) {
+		this.data[register] = value & BYTE;
+	}
+
+	public void setWord(int register, int value) {
+		this.data[register] = value & WORD;
+	}
+
+	public void setInt(int register, int value) {
+		this.data[register] = value;
+	}
 }
