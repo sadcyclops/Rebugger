cfee27d4b30d0eeb99b61addd9b69881b626907a
Eric Hemphill <erichemphill@Erics-MacBook-Pro.local>
2016-10-08 02:53:20 -0400

    Set up Machine Factory wiht fake machine.

--- src/frontEnd/www/js/TextEditorController.js
+++ src/frontEnd/www/js/TextEditorController.js
@@ -1,6 +1,8 @@
 rebuggerApp.controller('textEditorController', ['$scope', 'MachineFactory', function($scope, MachineFactory)
 {
 
+    $scope.lines = MachineFactory.machine.lines;
+    $scope.breakpoints = MachineFactory.breakpoints;
     $scope.fuckyou = 5;
 
 }]);
