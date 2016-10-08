f849b481a32156d478abdbee025ab154b5f0abfd
Eric Hemphill <erichemphill@Erics-MacBook-Pro.local>
2016-10-08 04:54:14 -0400

    Added text edtior features

--- src/frontEnd/www/js/TextEditorController.js
+++ src/frontEnd/www/js/TextEditorController.js
@@ -5,4 +5,10 @@ rebuggerApp.controller('textEditorController', ['$scope', 'MachineFactory', func
     $scope.breakpoints = MachineFactory.machine.breakpoints;
     $scope.currentLine = MachineFactory.machine.currentLine;
 
+
+    //add or remove the breakpoint form the breakpoints array
+    $scope.toggleBreakpoint = function (lineNum) {
+        $scope.breakpoints.indexOf(lineNum) > -1 ? $scope.breakpoints.push(lineNum) : $scope.breakpoints.splice($scope.breakpoints.indexOf(lineNum), 1);
+    }
+
 }]);
